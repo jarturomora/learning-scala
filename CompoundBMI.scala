e592b96e940af8130373dab44921c8f6dd3b6856
jarturomora <jarturomora@gmail.com>
2016-08-27 20:35:04 -0500

    Compoud Expression Atom done

---
+++ CompoundBMI.scala
@@ -0,0 +1,11 @@
+// Atom: Compound Expression
+// CompoundBMI.scala
+val lbs = 150.0
+val height = 68.0
+val weightStatus = {
+    val bmi = lbs /  (height * height) * 703.07
+    if (bmi < 18.5) "Underweight"
+    else if (bmi < 25) "Normal Weight"
+    else "Overweight"
+}
+println(weightStatus)
