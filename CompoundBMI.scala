6968ee9ccad46209767e484b411c7e6d59eae8ff
jarturomora <jarturomora@gmail.com>
2016-08-28 07:04:52 -0500

    All exercises from Summary 1 done

--- CompoundBMI.scala
+++ CompoundBMI.scala
@@ -2,10 +2,15 @@
 // CompoundBMI.scala
 val lbs = 150.0
 val height = 68.0
+var idealWeight = 0.0
 val weightStatus:Unit = {
     val bmi = lbs /  (height * height) * 703.07
     if (bmi < 18.5) { "Underweight" }
     else if (bmi < 25) { "Normal Weight" }
+    else if (bmi == 22) { idealWeight = bmi * (height * height) / 703.07}
     else { "Overweight" }
 }
 println(weightStatus)
+if (idealWeight != 0) {
+    println(idealWeight)
+}
