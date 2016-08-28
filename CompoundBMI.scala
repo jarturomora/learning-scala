3f3a6c3c7b1c5872b1328ce1896ea29ec16107ef
jarturomora <jarturomora@gmail.com>
2016-08-28 06:58:39 -0500

    Ex9 from Summary 1 done with the help of http://stackoverflow.com/questions/9461880/should-i-use-unit-or-leave-out-the-return-type-for-my-scala-method where the usage of Unit type as return value is explained quite good

--- CompoundBMI.scala
+++ CompoundBMI.scala
@@ -2,10 +2,10 @@
 // CompoundBMI.scala
 val lbs = 150.0
 val height = 68.0
-val weightStatus = {
+val weightStatus:Unit = {
     val bmi = lbs /  (height * height) * 703.07
-    if (bmi < 18.5) "Underweight"
-    else if (bmi < 25) "Normal Weight"
-    else "Overweight"
+    if (bmi < 18.5) { "Underweight" }
+    else if (bmi < 25) { "Normal Weight" }
+    else { "Overweight" }
 }
 println(weightStatus)
